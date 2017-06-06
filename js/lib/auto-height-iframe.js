0c71b6d76843c68ab4b59ff304ddc11ddcbc08f4
Mike Godin <mike@communicatehealth.com>
2017-06-06 14:33:22 -0400

    Add Auto-height iframe

---
+++ js/lib/auto-height-iframe.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+  
+  // While we are resizing, slow down the rate at which we send messages to the parent window
+  var resizeTimeoutId = -1;
+  function handleResize() {
+    resizeTimeoutId = -1;
+    parent.postMessage(($('body').outerHeight( true ) + 20 ) + 'px', '*');
+  }
+  // Here is the window resize handler
+  $(window).resize(function(){
+    if(resizeTimeoutId === -1){
+      resizeTimeoutId = setTimeout(handleResize,50);
+    }
+  });
+  // Fire off the resize message a few times as the page finishes rendering
+  setTimeout(handleResize,50);
+  setTimeout(handleResize,500);
+  setTimeout(handleResize,1000);
+  setTimeout(handleResize,2000);
+    
+});
