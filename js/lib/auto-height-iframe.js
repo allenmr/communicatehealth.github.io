7deaa22b2e3b6d6751a7ddc3e8375d2cd6475f82
Mike Godin <mike@communicatehealth.com>
2017-06-07 07:46:58 -0400

    Add code to handle resizing of inner document

--- js/lib/auto-height-iframe.js
+++ js/lib/auto-height-iframe.js
@@ -6,16 +6,17 @@ $( document ).ready(function() {
     resizeTimeoutId = -1;
     parent.postMessage($('html').outerHeight( true ) + 'px', '*');
   }
-  // Here is the window resize handler
-  $(window).resize(function(){
+  function pacedHandleResize() {
     if(resizeTimeoutId === -1){
       resizeTimeoutId = setTimeout(handleResize,50);
     }
-  });
-  // Fire off the resize message a few times as the page finishes rendering
-  setTimeout(handleResize,50);
-  setTimeout(handleResize,500);
-  setTimeout(handleResize,1000);
-  setTimeout(handleResize,2000);
-    
+  }
+  // Here is the window resize handler
+  $(window).resize(pacedHandleResize);
+  // Define an iframe over whole body to detect document content size changes
+  $('body').css('position','relative');
+  var apiframe = $('<iframe tabindex="-1" style="position:absolute;top:0;bottom:0;left:0;height:100%;width:0;border:0;background-color:transparent;"></iframe>').appendTo($('body'));
+  $(apiframe[0].contentWindow).resize(pacedHandleResize);
+  // Fire off at least one message
+  handleResize();
 });
