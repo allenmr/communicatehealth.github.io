ab604d90f233000afc674fc49788faf1dab31738
Jason Morris <jasonmorris@gmail.com>
2017-07-05 14:30:52 -0400

    Namespaced accordion

--- examples/js/accordion.js
+++ examples/js/accordion.js
@@ -2,27 +2,27 @@ jQuery( document ).ready(function( $ ) {
   "use strict";
 
   // close all accordion items when JS loads
-  $(".accordion-block").addClass("closed");
-  $(".accordion-toggle").attr("aria-expanded", "false");
-  $(".accordion-content").addClass("closed").attr("aria-hidden", "true");
+  $(".ch-accordion-block").addClass("ch-closed");
+  $(".ch-accordion-toggle").attr("aria-expanded", "false");
+  $(".ch-accordion-content").addClass("ch-closed").attr("aria-hidden", "true");
 
-  $(".accordion-toggle").click(function (event) {
+  $(".ch-accordion-toggle").click(function (event) {
     if (event.preventDefault) { event.preventDefault(); }
     else { event.returnValue = false; } // IE
 
-    var $next = $(this).next('.accordion-content');
+    var $next = $(this).next('.ch-accordion-content');
 
     // If accordion content is already visible, hide
     if ($(this).attr('aria-expanded') === 'true') {
       $(this).attr("aria-expanded", "false");
-      $(this).parent(".accordion-block").removeClass("open").addClass("closed");
+      $(this).parent(".ch-accordion-block").removeClass("ch-open").addClass("ch-closed");
       $.hideAnimate($next);
     }
 
     // Show accordion content
     else {
       $(this).attr("aria-expanded", "true");
-      $(this).parent(".accordion-block").removeClass("closed").addClass("open");
+      $(this).parent(".ch-accordion-block").removeClass("ch-closed").addClass("ch-open");
       $.showAnimate($next);
     }
   });
