4ef00488150e6fc45747e48b2d2b180a4ce3ce4a
Jason Morris <jasonmorris@gmail.com>
2017-07-05 14:41:43 -0400

    Namespaced slideshow

--- examples/js/slideshow.js
+++ examples/js/slideshow.js
@@ -1,66 +1,66 @@
 jQuery( document ).ready(function( $ ) {
   "use strict";
 
-  $(".content-slide-prev").click(function (event) {
+  $(".ch-content-slide-prev").click(function (event) {
     if (event.preventDefault) { event.preventDefault(); }
     else { event.returnValue = false; } // IE
 
-    var slideParent = $(this).parents('.content-slider-block').attr("id");
-    var prevItem = $("#" + slideParent + " .content-slide.on").prev();
+    var slideParent = $(this).parents('.ch-content-slider-block').attr("id");
+    var prevItem = $("#" + slideParent + " .ch-content-slide.ch-on").prev();
 
-    $("#" + slideParent + " .content-slide-prev").removeClass("limit");
-    $("#" + slideParent + " .content-slide-next").removeClass("limit");
-    $("#" + slideParent + " .content-slide-restart").addClass("limit");
+    $("#" + slideParent + " .ch-content-slide-prev").removeClass("ch-limit");
+    $("#" + slideParent + " .ch-content-slide-next").removeClass("ch-limit");
+    $("#" + slideParent + " .ch-content-slide-restart").addClass("ch-limit");
 
-    if(!$("#" + slideParent + " .content-slide.on").prev('.content-slide').prev('.content-slide').length) {
-      $("#" + slideParent + " .content-slide.on").removeClass("on").attr("aria-hidden", "true");
-      $(prevItem).addClass("on").attr("aria-hidden", "false");
-      $("#" + slideParent + " .content-slide-prev").addClass("limit");
+    if(!$("#" + slideParent + " .ch-content-slide.ch-on").prev('.ch-content-slide').prev('.ch-content-slide').length) {
+      $("#" + slideParent + " .ch-content-slide.ch-on").removeClass("ch-on").attr("aria-hidden", "true");
+      $(prevItem).addClass("ch-on").attr("aria-hidden", "false");
+      $("#" + slideParent + " .ch-content-slide-prev").addClass("ch-limit");
     }
     else {
-      $("#" + slideParent + " .content-slide.on").removeClass("on").attr("aria-hidden", "true");
-      $(prevItem).addClass("on").attr("aria-hidden", "false");
-      $("#" + slideParent + " .content-slide-prev").removeClass("limit");
+      $("#" + slideParent + " .ch-content-slide.ch-on").removeClass("ch-on").attr("aria-hidden", "true");
+      $(prevItem).addClass("ch-on").attr("aria-hidden", "false");
+      $("#" + slideParent + " .ch-content-slide-prev").removeClass("ch-limit");
     }
   });
 
-  $(".content-slide-next").click(function (event) {
+  $(".ch-content-slide-next").click(function (event) {
     if (event.preventDefault) { event.preventDefault(); }
     else { event.returnValue = false; } // IE
 
-    var slideParent = $(this).parents('.content-slider-block').attr("id");
-    var nextItem = $("#" + slideParent + " .content-slide.on").next();
+    var slideParent = $(this).parents('.ch-content-slider-block').attr("id");
+    var nextItem = $("#" + slideParent + " .ch-content-slide.ch-on").next();
 
-    $("#" + slideParent + " .content-slide-prev").removeClass("limit");
-    $("#" + slideParent + " .content-slide-next").removeClass("limit");
-    $("#" + slideParent + " .content-slide-restart").addClass("limit");
+    $("#" + slideParent + " .ch-content-slide-prev").removeClass("ch-limit");
+    $("#" + slideParent + " .ch-content-slide-next").removeClass("ch-limit");
+    $("#" + slideParent + " .ch-content-slide-restart").addClass("ch-limit");
 
-    if(!$("#" + slideParent + " .content-slide.on").next('.content-slide').next('.content-slide').length) {
-      $("#" + slideParent + " .content-slide.on").removeClass("on").attr("aria-hidden", "true");
-      $(nextItem).addClass("on").attr("aria-hidden", "false");
-      $("#" + slideParent + " .content-slide-next").addClass("limit");
-      $("#" + slideParent + " .content-slide-restart").removeClass("limit");
+    if(!$("#" + slideParent + " .ch-content-slide.ch-on").next('.ch-content-slide').next('.ch-content-slide').length) {
+      $("#" + slideParent + " .ch-content-slide.ch-on").removeClass("ch-on").attr("aria-hidden", "true");
+      $(nextItem).addClass("ch-on").attr("aria-hidden", "false");
+      $("#" + slideParent + " .ch-content-slide-next").addClass("ch-limit");
+      $("#" + slideParent + " .ch-content-slide-restart").removeClass("ch-limit");
     }
     else {
-      $("#" + slideParent + " .content-slide.on").removeClass("on").attr("aria-hidden", "true");
-      $(nextItem).addClass("on").attr("aria-hidden", "false");
-      $("#" + slideParent + " .content-slide-next").removeClass("limit");
-      $("#" + slideParent + " .content-slide-restart").addClass("limit");
+      $("#" + slideParent + " .ch-content-slide.ch-on").removeClass("ch-on").attr("aria-hidden", "true");
+      $(nextItem).addClass("ch-on").attr("aria-hidden", "false");
+      $("#" + slideParent + " .ch-content-slide-next").removeClass("ch-limit");
+      $("#" + slideParent + " .ch-content-slide-restart").addClass("ch-limit");
     }
   });
 
-  $(".content-slide-restart").click(function (event) {
+  $(".ch-content-slide-restart").click(function (event) {
     if (event.preventDefault) { event.preventDefault(); }
     else { event.returnValue = false; } // IE
 
-    var slideParent = $(this).parents('.content-slider-block').attr("id");
+    var slideParent = $(this).parents('.ch-content-slider-block').attr("id");
 
-    $("#" + slideParent + " .content-slide-prev").addClass("limit");
-    $("#" + slideParent + " .content-slide-next").removeClass("limit");
-    $("#" + slideParent + " .content-slide-restart").addClass("limit");
+    $("#" + slideParent + " .ch-content-slide-prev").addClass("ch-limit");
+    $("#" + slideParent + " .ch-content-slide-next").removeClass("ch-limit");
+    $("#" + slideParent + " .ch-content-slide-restart").addClass("ch-limit");
 
-    $("#" + slideParent + " .content-slide.on").removeClass("on").attr("aria-hidden", "true");
-    $("#" + slideParent + " .content-slide-first").addClass("on").attr("aria-hidden", "false");
+    $("#" + slideParent + " .ch-content-slide.ch-on").removeClass("ch-on").attr("aria-hidden", "true");
+    $("#" + slideParent + " .ch-content-slide-first").addClass("ch-on").attr("aria-hidden", "false");
 
   });
 
