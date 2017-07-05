ae20dc88540096eedd831b3980013d45cd1532cd
Jason Morris <jasonmorris@gmail.com>
2017-07-05 14:31:21 -0400

    Namespaced animated hide

--- examples/js/animated-hide.js
+++ examples/js/animated-hide.js
@@ -3,26 +3,26 @@ jQuery( document ).ready(function( $ ) {
 
   $.hideAnimate = function($element, $delay) {
     $delay = $delay === undefined ? 300 : $delay;
-    $element.addClass("closing");
-    $element.removeClass("open opening closed");
+    $element.addClass("ch-closing");
+    $element.removeClass("ch-open ch-opening ch-closed");
     setTimeout($.hideComplete, $delay, $element); // slower, allow for animation
   };
 
   $.hideComplete = function($element) {
-    $element.addClass("closed").attr("aria-hidden", "true");
-    $element.removeClass("open opening closing");
+    $element.addClass("ch-closed").attr("aria-hidden", "true");
+    $element.removeClass("ch-open ch-opening ch-closing");
   };
 
   $.showAnimate = function($element, $delay) {
     $delay = $delay === undefined ? 20 : $delay;
-    $element.addClass("opening");
-    $element.removeClass("open closing closed");
+    $element.addClass("ch-opening");
+    $element.removeClass("ch-open ch-closing ch-closed");
     setTimeout($.showComplete, $delay, $element); // faster, animation happens after
   };
 
   $.showComplete = function($element) {
-    $element.addClass("open").attr("aria-hidden", "false");
-    $element.removeClass("opening closing closed");
+    $element.addClass("ch-open").attr("aria-hidden", "false");
+    $element.removeClass("ch-opening ch-closing ch-closed");
   };
 
 });
