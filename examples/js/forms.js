24ea5007eb073a567d58d94122b17e7d1818ae0c
Jocelyn Cozzo <jocelyn@communicatehealth.com>
2017-09-08 17:07:13 -0400

    Forms - in progress
    HTML, some styles, missing validation

---
+++ examples/js/forms.js
@@ -0,0 +1,69 @@
+jQuery( document ).ready(function( $ ) {
+  "use strict";
+
+  // Auto formats phone number (instead of using a mask)
+  $('#phone').formatter({
+    'pattern': '({{999}}) {{999}}-{{9999}}',
+    'persistent': true //show pattern always "(###) ###-####"
+  });
+  
+  // https://jqueryui.com/autocomplete/
+  $( function() {
+    var availableStates = [
+      "Alabama",
+      "Alaska",
+      "Arizona",
+      "Arkansas",
+      "California",
+      "Colorado",
+      "Connecticut",
+      "Delaware",
+      "Florida",
+      "Georgia",
+      "Hawaii",
+      "Idaho",
+      "Illinois",
+      "Indiana",
+      "Iowa",
+      "Kansas",
+      "Kentucky",
+      "Louisiana",
+      "Maine",
+      "Maryland",
+      "Massachusetts",
+      "Michigan",
+      "Minnesota",
+      "Mississippi",
+      "Missouri",
+      "Montana",
+      "Nebraska",
+      "Nevada",
+      "New Hampshire",
+      "New Jersey",
+      "New Mexico",
+      "New York",
+      "North Carolina",
+      "North Dakota",
+      "Ohio",
+      "Oklahoma",
+      "Oregon",
+      "Pennsylvania",
+      "Rhode Island",
+      "South Carolina",
+      "South Dakota",
+      "Tennessee",
+      "Texas",
+      "Utah",
+      "Vermont",
+      "Virginia",
+      "Washington",
+      "West Virginia",
+      "Wisconsin",
+      "Wyoming"
+    ];
+    $( "#state" ).autocomplete({
+      source: availableStates
+    });
+  } );
+
+});
